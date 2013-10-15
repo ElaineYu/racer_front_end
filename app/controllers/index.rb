f291d5601cae075e281bc3d5fdfe8a960c55b596
Elaine Yu <elaineicecream@Elaines-MacBook-Pro.local>
2013-10-15 14:51:26 -0500

    double check

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,21 @@
+#1 .Starting /creating a new game
+
 get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+  # get two players to enter their information
+
+post '/process_users' do
+  @player1_initials = params[:player1][:initials]
+  @player2_initials = params[:player2][:initials]
+
+  erb :game
+end
+
+# 2. Playing a new game
+
+
+
+# 3. Viewing ther esults of an old game
